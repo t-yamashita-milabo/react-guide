f3001e97a927149a5aff29bd547b2170abd287a0
t-yamashita-milabo <t-yamashita@mi-labo.co.jp>
2022-07-25 14:33:27 +0900

    modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
-import Clock from "components/Clock";
-import EnhancedUserTable from "containers/UserTable";
+import {
+  Button,
+  useDisclosure,
+  Box,
+  Modal,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <div>
-      <h1>WEEEEEEEEEEEEEEEEEEEEEEEEEEEE</h1>
-      <Clock />
-      <EnhancedUserTable />
+      <Button onClick={onOpen}>OOOOOOOOOOOOOOOOOOOOOOOO</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box>XXXXXXXXXXXXXXXXXXXXXXXX</Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
